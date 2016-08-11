a66708d4c352e1b80a648580d3bfee35316cf0a0
Christian Diener <ch.diener@gmail.com>
2016-08-11 16:21:21 -0500

    Added new files and merged GDC branch

---
+++ tests/testthat/test_panel_stats.R
@@ -0,0 +1,15 @@
+## Copyright 2016 Christian Diener <mail[at]cdiener.com>
+##
+## MIT license. See LICENSE for more information.
+
+context("panel stats")
+
+manifest <- system.file("extdata", "manifest.tsv", package="tcgar")
+d <- tempdir()
+
+test_that("panel stats can be obtained", {
+    ps <- panel_samples(rnaseq=read_rnaseq(manifest, d, progress=FALSE),
+        huex=read_huex(manifest, d, progress=FALSE),
+        clinical=read_clinical(manifest, d, progress=FALSE))
+    expect_true(all(ps[1,-1] == c(2, 50, 1)))
+})
